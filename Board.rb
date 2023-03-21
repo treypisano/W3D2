daf96848559562b9297017d8b719e89b919598a0
joshua <joshk.5793@gmail.com>
2023-03-21 13:18:30 -0400

    added populate and random_positions

--- Board.rb
+++ Board.rb
@@ -1,5 +1,47 @@
 class Board
 
-  
+  def initialize()
+    @grid = Array.new(4) {Array.new(4)}
+  end
 
+  def populate
+    alpha = ("A".."Z").to_a
+    positions = self.random_positions
+    values = alpha.sample(8)
+    face_values = []
+
+    2.times do
+        face_values << values
+    end
+
+    face_values.each_with_index do |letter, idx1|
+        positions.each_with_index do |position, idx2|
+            if idx1 == idx2
+                @grid[position[0]][position[1]] = Card.new(letter, position)
+            end
+        end
+    end
+
+
+  end
+
+  def random_positions
+    positions = []
+
+    while positions.length < @grid.length**2
+        
+        first_number = rand(0...@grid.length)
+        second_number = rand(0...@grid.length)
+
+        new_position = [first_number,second_number]
+
+        if !positions.include?(new_position)
+            positions << new_position
+        end
+
+    end
+    positions
+
+
+  end
 end
--- Card.rb
+++ Card.rb
@@ -1,9 +1,13 @@
 class Card
-  
-  def initialize()
+  attr_reader :face_value, position_value
+
+  def initialize(face_value, position_value)
     @face_up = false
-    @face_value = ""
+    @face_value = face_value
+    @position_value = position_value
   end
+
+
 
   def hide
     if @face_value != ""
@@ -14,6 +18,14 @@ class Card
   def reveal
     if @face_value == " "
       @
+  end
+
+  def change_face
+    if @face_up == true
+      @face_up = false
+    else 
+      @face_up = true
+    end
   end
 
 end
