d1f4ebd6cff9bf54467b65faca74339dbb837d5f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-03-21 13:52:06 -0400

    made class and board

---
+++ Card.rb
@@ -0,0 +1,19 @@
+class Card
+  
+  def initialize()
+    @face_up = false
+    @face_value = ""
+  end
+
+  def hide
+    if @face_value != ""
+      @face_value = " "
+    end
+  end
+
+  def reveal
+    if @face_value == " "
+      @
+  end
+
+end
